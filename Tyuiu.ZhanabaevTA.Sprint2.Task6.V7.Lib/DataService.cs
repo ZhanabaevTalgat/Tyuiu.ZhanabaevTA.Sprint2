af3aa81c6a8524c33fbf2730be94b90d8f2f6a27
Talgat <talgatzhan297@yandex.ru>
2024-09-30 00:41:52 +0500

    Task6

---
+++ Tyuiu.ZhanabaevTA.Sprint2.Task6.V7.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ZhanabaevTA.Sprint2.Task6.V7.Lib
+{
+    public class DataService : ISprint2Task6V7
+    {
+        public string FindMonthName(int startYear, int n)
+        {
+            string MonthName = n switch
+            {
+                0 => "Январь",
+                1 => "Февраль",
+                2 => "Март",
+                3 => "Апрель",
+                4 => "Май",
+                5 => "Июнь",
+                6 => "Июль",
+                7 => "Август",
+                8 => "Сентябрь",
+                9 => "Октябрь",
+                10 => "Ноябрь",
+                11 => "Декабрь",
+                _ => "Неизвестный месяц" // Обработка неверного ввода
+            };
+
+            return MonthName;
+        }
+    }
+}
---
+++ Tyuiu.ZhanabaevTA.Sprint2.Task6.V7/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.ZhanabaevTA.Sprint2.Task6.V7.Lib;
+
+namespace Tyuiu.ZhanabaevTA.Sprint2.Task6.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Жанабаев Т.А | РППб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема:  Получение результата из swith                                    *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Жанабаев Талгат Асылбекович | РППб-24-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* С начала 1990 года по некоторый день прошло n месяцев и 2 дня.          *");
+            Console.WriteLine("* Определить название месяца (январь, февраль и т. п.) этого дня.         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            if (n < 0) { Console.WriteLine("Введено неверное значение"); }
+            else if (n > 11) { n %= 12; }
+
+            DataService ds = new DataService();
+            string MonthName = ds.FindMonthName(1990, n);
+            Console.WriteLine("Это месяц: " + MonthName);
+        }
+    }
+}
